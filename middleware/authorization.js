1ed5c1a32a7b0b9c10e89bb532272ab24d775420
amiribnjaman <amiribnjaman@gmail.com>
2024-12-11 13:55:30 +0600

    Authorization middleware create

---
+++ middleware/authorization.js
@@ -0,0 +1,28 @@
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../model/user.model");
+
+
+const authorization =  (req, res, next) => {
+  const auth = req.headers.authorization;
+  if (auth) {
+    const TOKEN = auth.split(" ")[1];
+    jwt.verify(TOKEN, process.env.ACCESS_TOKEN, TOKEN, async function (err, decoded) {
+      if (err) {
+        return res.send({ status: "401", msg: "Unathorized Access" });
+      } else {
+          const email = decoded;
+          const getuser = await User.findOne({ email: email });
+          if (getuser.role == 'admin') {
+              next();
+          } else {
+              return res.send({ status: 401, msg: "Unathorized Access!" });
+        }
+      }
+    });
+  } else {
+    return res.send({ status: 401, msg: "Unathorized Access!" });
+  }
+};
+
+module.exports = authorization;
--- route/vocabulary.router.js
+++ route/vocabulary.router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { createVocabulary, getAllVocabulary } = require("../controller/vocabulary.controller");
 const router = express.Router();
+const jwtAuthentication = require('../middleware/jwtAuthentication')
+const authorization = require('../middleware/authorization')
 
 // Router
-router.post("/createVocabulary", createVocabulary);
-router.get("/getAllVocabulary", getAllVocabulary);
+router.post("/createVocabulary", authorization, createVocabulary);
+router.get("/getAllVocabulary", jwtAuthentication, getAllVocabulary);
 
 module.exports = router;
